ee23a2b451a146be9bd55f0d3d76c1ef48daa16f
Adam Mertz <adammertz@gmail.com>
2017-11-22 22:01:41 -0600

    Wrote basis of stop command

--- cli/app.py
+++ cli/app.py
@@ -2,6 +2,7 @@
 
 import arrow
 import click
+import sys
 
 from bson.son import SON
 from pymongo import MongoClient
@@ -30,6 +31,32 @@ def list():
         last_seen = arrow.get(dev['timestamp']).format('YYYY-MM-DD HH:mm:ss')
         print(f'| {dev_id} | {last_seen} |')
     print('------------------------------------')
+
+
+@cli.command(help='Send stop command to device')
+@click.argument('device_id')
+def stop(device_id):
+    device_list = db.bot_logs.distinct('device_id')
+
+    n = len(device_id)
+    target_ids = []
+    for dev in device_list:
+        if dev[:n] == device_id:
+            target_ids.append(dev)
+
+    if not target_ids:
+        print(f'Device: "{device_id}" not found')
+        print('use "bots list" to print list of devices')
+        sys.exit()
+
+    if len(target_ids) > 1:
+        print(f'Multiple targets found for "{device_id}" be more specific')
+        for dev_id in target_ids:
+            print(f'device_id: {dev_id}')
+        sys.exit()
+
+    target_id = target_ids.pop()
+    print(f'Stop command sent to: {target_id}')
 
 
 if __name__ == '__main__':
