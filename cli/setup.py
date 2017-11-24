ac3efbb8d6451dd3f037971c99257577ff902107
Adam Mertz <adammertz@gmail.com>
2017-11-24 09:23:35 -0600

    Changed requirements to be installed via a requirements.txt file since the app is installed as a package.

--- cli/setup.py
+++ cli/setup.py
@@ -1,9 +1,15 @@
 from setuptools import setup
+from pip.req import parse_requirements
+
+install_reqs = parse_requirements('requirements.txt', session='hack')
+
+reqs = [str(ir.req) for ir in install_reqs]
 
 setup(
     name='bots-cli',
     version='0.1',
     py_modules=['app'],
+    install_requires=reqs,
     entry_points='''
         [console_scripts]
         bots=app:main
