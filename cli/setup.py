13af9e9ebc6a40f808d2ff52a4e53d029ed4cda2
Adam Mertz <adammertz@gmail.com>
2017-11-24 09:57:24 -0600

    Got rid of requirements.txt and simply built list in setup.py. Used python dict instead of string to write entry_points.

--- cli/setup.py
+++ cli/setup.py
@@ -1,17 +1,17 @@
 from setuptools import setup
-from pip.req import parse_requirements
-
-install_reqs = parse_requirements('requirements.txt', session='hack')
-
-reqs = [str(ir.req) for ir in install_reqs]
 
 setup(
     name='bots-cli',
     version='0.1',
     py_modules=['app'],
-    install_requires=reqs,
-    entry_points='''
-        [console_scripts]
-        bots=app:main
-    ''',
+    install_requires=[
+        'arrow==0.10.0',
+        'pika==0.11.0',
+        'pymongo==3.5.1'
+    ],
+    entry_points={
+        'console_scripts': [
+            'bots = app:main'
+        ]
+    },
 )
